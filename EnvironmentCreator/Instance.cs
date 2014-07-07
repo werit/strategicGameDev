b75e09b4b97e294dbad0b356e1cfc1e00d39b520
werit <tib.balaz@gmail.com>
2014-07-07 05:19:49 +0200

    Function and instance class added.

---
+++ EnvironmentCreator/Instance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentCreator
+{
+    class Instance
+    {
+        private Types m_instanceOf;
+        /** @brief List of planned actions of instance in  order.
+         * Variable storing all actions that are about to happen.
+         * Possible to be empty in case no action prepared.
+         */
+        public List<Action> m_actionQue = new List<Action>();
+
+    }
+}
